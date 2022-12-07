de17191b70b3e9ec76e40be4d12fe0e7444381c0
Ashwin Kumar <AKumar_contractor@claimscorpinc.com>
2022-12-06 20:17:31 -0700

    test

--- kubernetes-2node-deployment/step1/verify.sh
+++ kubernetes-2node-deployment/step1/verify.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-repResult="$(k get deploy nginx-depl -o=jsonpath='{.status.replicas}')";
-image="$(k get deploy nginx-depl -o=jsonpath='{.spec.template.spec.containers[0].image}')";
-if [ "$(k get deploy nginx-depl)" ] && [ "$repResult" -eq 2 ] && [ "$image" = "nginx" ]
+if [ "$(k get deploy nginx-depl)" ] && [ "$(k get deploy nginx-depl -o=jsonpath='{.status.replicas}')" -eq 2 ] && [ "$(k get deploy nginx-depl -o=jsonpath='{.spec.template.spec.containers[0].image}')" = "nginx" ]
 then
    exit 1
 fi

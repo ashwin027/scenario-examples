19221e140693f4d799c736babface9ff4e526c17
Ashwin Kumar <AKumar_contractor@claimscorpinc.com>
2022-12-07 11:36:57 -0700

    Moved everything to one line

--- kubernetes-2node-deployment/step1/verify.sh
+++ kubernetes-2node-deployment/step1/verify.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-if [ ! "$(k get deploy nginx-depl)" ] || [ ! "$(k get deploy nginx-depl -o=jsonpath='{.status.replicas}')" -eq 2 ] || [ ! "$(k get deploy nginx-depl -o=jsonpath='{.spec.template.spec.containers[0].image}')" = "nginx" ]
-then
-   exit 1;
-fi
+if [ ! "$(k get deploy nginx-depl)" ] || [ ! "$(k get deploy nginx-depl -o=jsonpath='{.status.replicas}')" -eq 2 ] || [ ! "$(k get deploy nginx-depl -o=jsonpath='{.spec.template.spec.containers[0].image}')" = "nginx" ]; then exit 1; fi

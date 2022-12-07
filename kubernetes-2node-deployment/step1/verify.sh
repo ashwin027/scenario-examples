2f0fe4ca62effcf4505a532a84bd93ba8540ce3b
Ashwin Kumar <AKumar_contractor@claimscorpinc.com>
2022-12-07 11:23:04 -0700

    Inverted if

--- kubernetes-2node-deployment/step1/verify.sh
+++ kubernetes-2node-deployment/step1/verify.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ "$(k get deploy nginx-depl)" ] && [ "$(k get deploy nginx-depl -o=jsonpath='{.status.replicas}')" -eq 2 ] && [ "$(k get deploy nginx-depl -o=jsonpath='{.spec.template.spec.containers[0].image}')" = "nginx" ]
+if ![ "$(k get deploy nginx-depl)" ] && [ "$(k get deploy nginx-depl -o=jsonpath='{.status.replicas}')" -eq 2 ] && [ "$(k get deploy nginx-depl -o=jsonpath='{.spec.template.spec.containers[0].image}')" = "nginx" ]
 then
-   echo "Success"
-else
    exit 1
 fi

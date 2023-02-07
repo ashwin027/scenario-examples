41a0c6c7b27ee2ca028f3f1fa135b0e9a535d365
Ashwin <ashwin027@gmail.com>
2023-02-07 10:10:06 -0700

    changed k to kubectl

--- kubernetes-2node-deployment/step1/verify.sh
+++ kubernetes-2node-deployment/step1/verify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if [ ! "$(kubectl get deploy nginx-depl)" ] || [ ! "$(kubectl get deploy nginx-depl -o=jsonpath='{.status.replicas}')" -eq 2 ] || [ ! "$(kubectl get deploy nginx-depl -o=jsonpath='{.spec.template.spec.containers[0].image}')" = "nginx" ]; then exit 1; fi
+if [ ! "$(kubectl get deploy nginx-depl)" ] || [ ! "$(kubectl get deploy nginx-depl -o=jsonpath='{.status.replicas}')" -eq 2 ] || [ ! "$(kubectl get deploy nginx-depl -o=jsonpath='{.spec.template.spec.containers[0].image}')" = "nginx" ]; then exit 1; else exit 0; fi

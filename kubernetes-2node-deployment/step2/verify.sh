223d0069ddb8fdb43b8f820c8b77b6832b020cec
Ashwin <ashwin027@gmail.com>
2023-02-07 09:54:12 -0700

    Changed k to kubectl

--- kubernetes-2node-deployment/step2/verify.sh
+++ kubernetes-2node-deployment/step2/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-repResult="$(k get deploy nginx-depl -o=jsonpath='{.status.replicas}')";
+repResult="$(kubectl get deploy nginx-depl -o=jsonpath='{.status.replicas}')";
 if [ "$repResult" -eq 1 ]
 then
    exit 0
